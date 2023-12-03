9b1fafdcf9c139b7c50db9071483c05ec0976af7
Pablo Santos <123404411+thepablosantos@users.noreply.github.com>
2023-12-03 14:14:52 +0000

    Update gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -1,20 +1,61 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Carregando dados do arquivo CSV
+# Carregando os dados do arquivo gasolina.csv
 df = pd.read_csv('gasolina.csv')
 
-# Criando gráfico de linha
+# Configurando o estilo do Seaborn (opcional)
+sns.set(style="whitegrid")
+
+# Criando o gráfico de linha
 plt.figure(figsize=(10, 6))
-plt.plot(df['dia'], df['venda'], marker='o', linestyle='-', color='b')
+sns.lineplot(x='dia', y='venda', data=df, marker='o', color='blue')
 
 # Adicionando rótulos e título
-plt.title('Preço da Gasolina ao Longo dos Dias')
 plt.xlabel('Dia')
-plt.ylabel('Preço (R$)')
+plt.ylabel('Preço de Venda (R$)')  # Modificado para refletir a unidade monetária
+plt.title('Variação do Preço de Venda de Gasolina ao Longo dos Dias')
 
-# Salvando o gráfico em um arquivo PNG
+# Adicionando uma legenda (opcional)
+plt.legend(['Preço de Venda'])
+
+# Salvando o gráfico como um arquivo PNG
 plt.savefig('gasolina.png')
 
-# Exibindo o gráfico
+# Mostrando o gráfico (opcional)
 plt.show()
+
+#  Salvando o código Python em um arquivo
+code = """
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Carregando os dados do arquivo gasolina.csv
+df = pd.read_csv('gasolina.csv')
+
+# Configurando o estilo do Seaborn (opcional)
+sns.set(style="whitegrid")
+
+# Criando o gráfico de linha
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='dia', y='venda', data=df, marker='o', color='blue')
+
+# Adicionando rótulos e título
+plt.xlabel('Dia')
+plt.ylabel('Preço de Venda (R$)')  # Modificado para refletir a unidade monetária
+plt.title('Variação do Preço de Venda de Gasolina ao Longo dos Dias')
+
+# Adicionando uma legenda (opcional)
+plt.legend(['Preço de Venda'])
+
+# Salvando o gráfico como um arquivo PNG
+plt.savefig('gasolina.png')
+
+# Mostrando o gráfico (opcional)
+plt.show()
+"""
+
+with open('gasolina.py', 'w') as file:
+    file.write(code)
